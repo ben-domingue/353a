47b110884292c88f7e95513fdb92bd7b2a680b45
ben-domingue <ben.domingue@gmail.com>
2023-01-03 09:01:48 -0800

    ignore

--- notyet/elo.R
+++
@@ -1,6 +0,0 @@
-https://cran.r-project.org/web/packages/elo/vignettes/comparison_methods.html
-https://cran.r-project.org/web/packages/elo/index.html #see other vignettes
-https://cran.r-project.org/web/packages/EloRating/vignettes/EloRating_tutorial.pdf
-
-
-#elo with ties
