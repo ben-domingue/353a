eb4ff713a3d57c64ea234aab00dff833bd2aff8e
Ben Domingue <ben.domingue@gmail.com>
2025-02-05 20:42:18 -0800

    cdm

--- cdm_hierarchical.R
+++ cdm_hierarchical.R
@@ -1,14 +1,10 @@
 library(GDINA)
 ##https://doi.org/10.18637/jss.v093.i14
 
-frac20$dat
-frac20$Q
-
-dat <- sim10GDINA$simdat
-Q <- sim10GDINA$simQ
-                                        # --- Higher order G-DINA model ---#
-mod12 <- GDINA(dat = dat, Q = Q, model = "DINA",
+m <- GDINA(frac20$dat,frac20$Q, model = "DINA",
                att.dist="higher.order",higher.order=list(nquad=31,model = "2PL"))
-th<-personparm(mod12,"HO") # higher-order ability
-map<-personparm(mod12,"MAP")
+
+th<-personparm(m,"HO") # higher-order ability
+map<-personparm(m,"MAP")
 cor(data.frame(th=th[,1],map[,1:3])) ##what do you think?
+coef(m,'lambda') ##how do these look relative to what 
