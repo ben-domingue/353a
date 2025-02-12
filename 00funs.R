76a0ca5c9090855971a67feab90861957842e8d1
Ben Domingue <ben.domingue@gmail.com>
2025-02-12 11:11:46 -0800

    cdm

---
+++ 00funs.R
@@ -0,0 +1,151 @@
+irt.pr<-function(resp,modeltype='Rasch',th.type='EAP') {
+    ##get irt pvalues
+    library(mirt)
+    m<-mirt(resp,1,modeltype)
+    th<-fscores(m,,method=th.type)
+    co<-coef(m,IRTpars=TRUE,simplify=TRUE)$items
+    x<-list()
+    for (i in 1:nrow(co)) {
+        z<-co[i,1]*(th[,1]-co[i,2])
+        x[[i]]<-1/(1+exp(-1*z))
+    }
+    do.call("cbind",x)
+}
+
+## cdm.pr<-function(resp,qm,modeltype="DINA") {
+##     ##cdm pvalues
+##     library(GDINA)
+##     m <- GDINA(resp,qm,model=modeltype)
+##     map <- personparm(m, what = "EAP")[,1:ncol(qm)]
+##     gs<-coef(m,what='gs')
+##     p<-list()
+##     for (i in 1:nrow(qm)) {
+##         ii<-which(qm[i,]==1)
+##         z<-map[,ii,drop=FALSE]
+##         rm<-rowMeans(z)
+##         p[[i]]<-ifelse(rm==1,1-gs[i,2],gs[i,1])
+##     }
+##     p.cdm<-do.call("cbind",p)
+## }
+
+oos.compare<-function(resp,qm,nfolds=5,modeltype) {
+    id<-1:nrow(resp)
+    item<-names(resp)
+    L<-list()
+    for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],resp=resp[,i])
+    df<-do.call("rbind",L)
+    df$gr<-sample(1:nfolds,nrow(df),replace=TRUE)
+    
+    om<-numeric()
+    for (gr in unique(df$gr)) {
+        oos<-df[df$gr==gr,]
+        ins<-df[df$gr!=gr,]
+        x<-irwpkg::irw_long2resp(ins)
+        id<-x$id
+        x<-x[,names(resp)]
+        ##
+        p.cdm<-cdm.pr.marg(x,qm)
+        L<-list()
+        for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],p.cdm=p.cdm[,i])
+        p<-do.call("rbind",L)
+        oos<-merge(oos,p)
+        ##
+        p.irt<-irt.pr(x,,modeltype=modeltype)
+        L<-list()
+        for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],p.irt=p.irt[,i])
+        p<-do.call("rbind",L)
+        oos<-merge(oos,p)
+        ##
+        om[gr]<-imv::imv.binary(oos$resp,oos$p.irt,oos$p.cdm)
+    }
+    mean(om)
+}
+
+
+oos.compare.newresp<-function(resp,qm,truep) {
+    id<-1:nrow(resp)
+    item<-names(resp)
+    L<-list()
+    for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],resp=resp[,i],truep=truep[,i])
+    df<-do.call("rbind",L)
+    om<-numeric()
+    x<-irwpkg::irw_long2resp(df)
+    id<-x$id
+    x<-x[,names(resp)]
+    ##
+    p.cdm<-cdm.pr.marg(x,qm)
+    L<-list()
+    for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],p.cdm=p.cdm[,i])
+    p<-do.call("rbind",L)
+    df<-merge(df,p)
+    ##
+    p.irt<-irt.pr(x)
+    L<-list()
+    for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],p.irt=p.irt[,i])
+    p<-do.call("rbind",L)
+    df<-merge(df,p)
+    ##
+    df$resp<-rbinom(nrow(df),1,df$truep) ##obliterate old response
+    om<-imv::imv.binary(df$resp,df$p.irt,df$p.cdm)
+    oracle.irt<-imv::imv.binary(df$resp,df$p.irt,df$truep)
+    oracle.cdm<-imv::imv.binary(df$resp,df$p.cdm,df$truep)
+    ##
+    rms<-function(x) sqrt(mean(x^2))
+    rms.irt<-rms(df$resp-df$p.irt)
+    rms.cdm<-rms(df$resp-df$p.cdm)
+    rms.irt.p<-rms(df$truep-df$p.irt)
+    rms.cdm.p<-rms(df$truep-df$p.cdm)
+    ##
+    c(om=om,oracle.irt=oracle.irt,oracle.cdm=oracle.cdm,
+      rms.irt=rms.irt,rms.cdm=rms.cdm,rms.irt.p=rms.irt.p,rms.cdm.p=rms.cdm.p)
+}
+
+
+oos.compare.2q<-function(resp,qm1,qm2,nfolds=5) {
+    id<-1:nrow(resp)
+    item<-names(resp)
+    L<-list()
+    for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],resp=resp[,i])
+    df<-do.call("rbind",L)
+    df$gr<-sample(1:nfolds,nrow(df),replace=TRUE)
+    
+    om<-numeric()
+    for (gr in unique(df$gr)) {
+        oos<-df[df$gr==gr,]
+        ins<-df[df$gr!=gr,]
+        x<-irwpkg::irw_long2resp(ins)
+        id<-x$id
+        x<-x[,names(resp)]
+        ##
+        p.cdm<-cdm.pr.marg(x,qm1)
+        L<-list()
+        for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],p.cdm1=p.cdm[,i])
+        p<-do.call("rbind",L)
+        oos<-merge(oos,p)
+        ##
+        p.cdm<-cdm.pr.marg(x,qm2)
+        L<-list()
+        for (i in 1:ncol(resp)) L[[i]]<-data.frame(id=id,item=names(resp)[i],p.cdm2=p.cdm[,i])
+        p<-do.call("rbind",L)
+        oos<-merge(oos,p)
+        ##
+        om[gr]<-imv::imv.binary(oos$resp,oos$p.cdm1,oos$p.cdm2)
+    }
+    mean(om)
+}
+
+cdm.pr.marg<-function(resp,qm,modeltype="DINA") {
+  ##cdm pvalues
+  library(GDINA)
+  m <- GDINA(resp,qm,modeltype)
+  map <- personparm(m, what = "mp")[,1:ncol(qm)]
+  gs<-coef(m,what='gs')
+  p<-list()
+  for (i in 1:nrow(qm)) {
+    ii<-which(qm[i,]==1)
+    z<-map[,ii,drop=FALSE]
+    rm<-apply(z,1,prod)
+    p[[i]]<-(1-gs[i,2])*rm + (1-rm)*gs[i,1]
+  }
+  p.cdm<-do.call("cbind",p)
+}
---
+++ scenario1.R
@@ -0,0 +1,93 @@
+source("00funs.R") ##https://github.com/AnyaWMa/IRW-Qmatrix/blob/main/bd/00funs.R
+
+
+################################3
+##simulate data with naughty cdm
+cdm.sim<-function(a,N=1000,sk.offset=0,nsk=6,bound=NULL) {
+    ##skills
+    th<-rnorm(N)
+    sk<-runif(nsk)
+    p<-a*(outer(th,sk-mean(sk),'-'))
+    p<-p+sk.offset ##controlling prevalence of skills
+    p<-apply(p,2,function(x) 1/(1+exp(-(x))))
+    sk<-apply(p,2,function(x) rbinom(nrow(p),1,p))
+    ##qmatrix
+    S<-TRUE
+    while (S) {
+        qm<-matrix(rbinom(50*nsk,1,.65),50,nsk)
+        S<-any(c(colMeans(qm),rowMeans(qm))==0)
+    }        
+    ##response probabilities 
+    pL<-respL<-list()
+    if (is.null(bound)) {
+        g<-runif(nrow(qm),min=0,max=.35)
+        s<-runif(nrow(qm),min=0,max=.35)
+    } else {
+        g<-rep(bound,nrow(qm))
+        s<-rep(bound,nrow(qm))
+    }
+    for (i in 1:nrow(qm)) {
+        ii<-which(qm[i,]==1)
+        z<-sk[,ii,drop=FALSE]
+        rm<-rowMeans(z)
+        p.tmp<-ifelse(rm==1,1-s[i],g[i])
+        respL[[i]]<-rbinom(N,1,p.tmp)
+        pL[[i]]<-p.tmp
+    }
+    resp<-do.call("cbind",respL)
+    p.true<-do.call("cbind",pL)
+    resp<-as.data.frame(resp)
+    names(resp)<-paste("i",1:ncol(resp),sep='')
+    ##cv imv values
+    om<-oos.compare.newresp(resp,qm,truep=p.true)
+    ##
+    om
+}
+a<-sort(runif(100,min=0,max=3))
+library(parallel)
+
+out2<-list()
+for (i in c(.1,.2,.3)) out2[[as.character(i)]]<-mclapply(a,cdm.sim,mc.cores=10,sk.offset=1.5,bound=i)
+
+
+
+#pdf("/home/bdomingu/Dropbox/Apps/Overleaf/CDM_predictions/scenario1.pdf",width=6,height=3)
+par(mgp=c(2,1,0),mfrow=c(3,2),mar=c(3,3,1,1),oma=rep(.5,4))
+####
+for (i in 1:length(out2)) {
+    plot(NULL,xlim=c(0,3),ylim=c(0,.6),xlab='a',ylab='RMSE(oos resp,est)')
+    pf<-function(x,y,...) {
+        m<-loess(y~x)
+        lines(x,predict(m),...,lwd=2)
+    }
+    f<-function(out,...) {
+        z<-do.call("rbind",out)
+        irt<-z[,4]
+        cdm<-z[,5]
+        irt.p<-z[,6]
+        cdm.p<-z[,7]
+        pf(a,irt,col='blue',...)
+        pf(a,cdm,col='red',...)
+        pf(a,irt.p,col='blue',...,lty=2)
+        pf(a,cdm.p,col='red',...,lty=2)
+    }
+                                        #f(out1)
+    f(out2[[i]])
+    legend("topright",bty='n',lty=c(1,1,2,2),col=c("blue","red","blue","red"),cex=.7,
+           c("(resp,IRT)","(resp,CDM)","(True,IRT)","(True,CDM)"))
+#####
+    plot(NULL,xlim=c(0,3),ylab="IMV",xlab='a',ylim=c(0,.15))
+    f<-function(out,...) {
+        om<-do.call("rbind",out)
+        abline(h=0)
+        pf(a,om[,1],...)
+        pf(a,om[,2],col='blue',...,lty=2)
+        pf(a,om[,3],col='red',...,lty=2)
+    }
+    f(out2[[i]])
+    legend("topright",bty='n',lty=c(1,2,2),col=c("black","blue","red"),cex=.7,
+           c("(IRT,CDM)","(IRT,True)","(CDM,True)")
+           )
+}
+##legend("topright",bty='n',lwd=1,lty=c(1,2),title=expression(delta),legend=c(0,1.5))
+##dev.off()
