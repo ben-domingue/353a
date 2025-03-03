58668747c9a04bcd9a503033c1a91b6dd9451630
Ben Domingue <ben.domingue@gmail.com>
2025-03-03 05:15:03 -0800

    ...

--- irtree_imv_LINvNEST.R
+++ irtree_imv_LINvNEST.R
@@ -2,12 +2,51 @@ library("irtrees")
 library("lme4")
 library(imv) #devtools::install_github("ben-domingue/imv", ref="main")
 
-df<-irwpkg::irw_fetch("teq_novak_2021_selfesteem")
+
+imv_c<-function(y,pctt.tab,p1,p2,sigma=1e-4) {
+  nn<-length(unique(y$resp))
+  om<-numeric()
+  iis<-0:(nn-1)
+  for (ii in iis) {
+    ns<-om.tmp<-numeric()
+    jjs<-iis[-match(ii,iis)]
+    for (jj in jjs) {
+      y2<-y[y$resp %in% c(ii,jj),]
+      resp<-ifelse(y2$resp==ii,1,0)
+      ##irt p-values for being
+      p1.ii<-y2[[paste(p1,ii,sep='')]]
+      p1.jj<-y2[[paste(p1,jj,sep='')]]
+      p2.ii<-y2[[paste(p2,ii,sep='')]]
+      p2.jj<-y2[[paste(p2,jj,sep='')]]
+      ##
+      z<-data.frame(resp=resp,
+                    p1=p1.ii/(p1.ii+p1.jj),
+                    p2=p2.ii/(p2.ii+p2.jj)
+      )
+      j0<-as.character(jj)
+      ##
+      z[,2]<-ifelse(z[,2]<sigma,sigma,z[,2])
+      z[,3]<-ifelse(z[,3]<sigma,sigma,z[,3])
+      z[,2]<-ifelse(z[,2]>1-sigma,1-sigma,z[,2])
+      z[,2]<-ifelse(z[,3]>1-sigma,1-sigma,z[,3])
+      ##
+      om.tmp[j0]<-imv::imv.binary(z[,1],z[,2],z[,3])
+      ns[as.character(jj)]<-nrow(z)
+    }
+    om[ii+1]<-sum(om.tmp*ns)/sum(ns)
+  }
+  omega_c <- sum(om*pctt.tab)/sum(pctt.tab)
+  return(omega_c)
+}
+
+df<-irwpkg::irw_fetch("promis1wave1_cesd")
 irw.table<-irwpkg::irw_long2resp(df)
 irw.table$id<-NULL
+rs<-rowSums(is.na(irw.table))
+irw.table<-irw.table[rs==0,]
 data<-as.matrix(irw.table)
 
-data<-linresp
+#data<-nesresp ##Note the code won't work below for linresp as it has responses in K=3 categories and a lot of what is below is dependent on K=4
 ################################################################################
 ##IMV for polytomous response over 1 fold
 ##nested versus linear
@@ -48,12 +87,11 @@ om<-numeric()
 for (fold in 1:4) {
     print(fold)
     ## The unidimensional model for linear response trees
-    m.lin <- glmer(resp ~ 0 + item:node + (0+node | id), 
+    m.lin <- glmer(resp ~ 0 + item:node + (1 | id), 
                    family = binomial, data = x.lin[x.lin$gr!=fold,])
-    ## The unidimensional model for linear response trees
-    m.nest <- glmer(resp ~ 0 + item:node + (0+node | id), 
+    ## The unidimensional model for nested response trees
+    m.nest <- glmer(resp ~ 0 + item:node + (1 | id), 
                     family = binomial, data = x.nest[x.nest$gr!=fold,])
-
     ##add nodes & compute category probs
     ##linear
     oos<-x.lin[x.lin$gr==fold,]
@@ -98,7 +136,6 @@ for (fold in 1:4) {
     }
     tab.lin<-f(L,'p0')
     names(tab.lin)<-paste("mlin.",0:3,sep='')
-
     ##nested
     oos<-x.nest[x.nest$gr==fold,]
     zz<-oos[oos$node=='node2',]
@@ -132,7 +169,6 @@ for (fold in 1:4) {
     }
     tab.nest<-f(L,'p0')
     names(tab.nest)<-paste("mnest.",0:3,sep='')
-
     ##get out-of-sample responses
     L<-split(oos,oos$bigid)
     f<-function(x) {
@@ -149,14 +185,14 @@ for (fold in 1:4) {
     }
     resp.oos<-sapply(L,f)
     y<-data.frame(resp=resp.oos,tab.lin,tab.nest)
-
+#
     resp<-as.numeric(data) ##not quite right, should be purely out of sample
     pctt.tab <- c()
     for (i in 0:(length(unique(resp))-1)){ #I got lazy and construct `pctt.tab` using all responses (should be OOS)
         pctt.tab <- c(pctt.tab, sum(resp == i)/length(resp))
     }
-
     om[fold]<-imv_c(y,pctt.tab,p1='mlin.',p2='mnest.')
+    print(om)
 }
 summary(om)
 
